343ec487aa36bf9b88d391231f8cec2e492230a5
Alexandru Bese <alexandru.bese@3pillarglobal.com>
2022-02-10 09:21:48 +0200

    Fixed imports

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -15,8 +15,8 @@ import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 import { Task } from './task.entity';
 import { TasksService } from './tasks.service';
-import { GetUser } from 'src/auth/get-user.decorator';
-import { User } from 'src/auth/user.entity';
+import { GetUser } from '../auth/get-user.decorator';
+import { User } from '../auth/user.entity';
 
 @Controller('tasks')
 @UseGuards(AuthGuard())
